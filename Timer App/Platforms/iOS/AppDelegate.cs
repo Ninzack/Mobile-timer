da2da2c55e25a80558400d054ca4f2a07836d6d5
Ninzack <113981273+Ninzack@users.noreply.github.com>
2023-12-13 10:12:23 +0100

    From .NET 7.0 to .NET 8.0

---
+++ Timer App/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Timer_App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
